7428ee7ff926b9b15b981e6a4469870720de7a16
guiganassini17@gmail.com <guiganassini17@gmail.com>
2024-12-15 23:33:25 -0300

    tarefa 1

--- tarefas-praticas/1/118-casa.c
+++ tarefas-praticas/1/118-casa.c
@@ -1,36 +1,20 @@
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
-    int* array = (int*)malloc(sizeof(int));
-    int** returnArray = (int**)malloc(sizeof(int*) * numRows);
-    int j;
+    int** returnArray = (int**)malloc(numRows * sizeof(int*));
+	int i, j;
 
-    for (int i = 0; i < numRows; i++) {
-        j = 1;
+    for ( i = 0; i < numRows; i++ ) {
+        returnArray[i] = (int*)malloc((i + 1) * sizeof(int));
 
-        if ( i == 0 ) {
-            array[0] = 1;
-            returnArray[0] = array;
-            continue;
-        }
+        returnArray[i][0] = 1;
 
-        if ( i == 1 ) {
-            array = (int*)realloc(array, sizeof(int)*2);
-            returnArray = (int**)realloc(returnArray, sizeof(int*)*2);
-            array[0] = 1;
-            array[1] = 1;
-            returnArray[1] = array;
-            continue;
-        }
+		for ( j = 1; j < i; j++ ) {
 
-        array = (int*)realloc(array, sizeof(int)*2);
-        array[0] = 1;
-
-        while ( j < i ) {
-            array[j] = returnArray[i-1][j] + returnArray[i-1][j + 1];
-            j++;
-        }
-
-        array[j+1] = 1;
+			returnArray[i][j] = returnArray[i-1][j-1] + returnArray[i-1][j];
+		}
+        
+		returnArray[i][i] = 1;
     }
 
     return returnArray;
 }
+
